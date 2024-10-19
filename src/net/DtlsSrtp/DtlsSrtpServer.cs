fb3c0ff8c00d872ffc4ed01606fd75b98a54b7d2
Victor Nova <victor@borg.games>
2024-10-19 14:10:41 -0700

    IDtlsSrtpPeer was originally developed in SIPSorcery

--- src/net/DtlsSrtp/DtlsSrtpServer.cs
+++ src/net/DtlsSrtp/DtlsSrtpServer.cs
@@ -30,19 +30,6 @@ using SIPSorcery.Sys;
 
 namespace SIPSorcery.Net
 {
-    public interface IDtlsSrtpPeer
-    {
-        event Action<DtlsAlertLevel, DtlsAlertType, string> OnAlert;
-        bool ForceUseExtendedMasterSecret { get; set; }
-        SrtpPolicy GetSrtpPolicy();
-        SrtpPolicy GetSrtcpPolicy();
-        byte[] GetSrtpMasterServerKey();
-        byte[] GetSrtpMasterServerSalt();
-        byte[] GetSrtpMasterClientKey();
-        byte[] GetSrtpMasterClientSalt();
-        bool IsClient();
-        Certificate GetRemoteCertificate();
-    }
 
     public class DtlsSrtpServer : DefaultTlsServer, IDtlsSrtpPeer
     {
---
+++ src/net/DtlsSrtp/IDtlsSrtpPeer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Org.BouncyCastle.Tls;
+
+namespace SIPSorcery.Net;
+
+public interface IDtlsSrtpPeer
+{
+    event Action<DtlsAlertLevel, DtlsAlertType, string> OnAlert;
+    bool ForceUseExtendedMasterSecret { get; set; }
+    SrtpPolicy GetSrtpPolicy();
+    SrtpPolicy GetSrtcpPolicy();
+    byte[] GetSrtpMasterServerKey();
+    byte[] GetSrtpMasterServerSalt();
+    byte[] GetSrtpMasterClientKey();
+    byte[] GetSrtpMasterClientSalt();
+    bool IsClient();
+    Certificate GetRemoteCertificate();
+}
